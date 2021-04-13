a3d74cce9abc419aef836c4dad50b753d9a52362
Eric Janero <ejanerop97@gmail.com>
2021-04-13 00:14:52 -0400

    installed angular flex layout created components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login' , component : LoginComponent},
+  /*{path : 'list' , component : ListComponent , canActivate : [AuthGuard]},*/
+  {path : 'home' , component : HomeComponent},
+  {path : 'user' , component : UserListComponent},
+  {path : 'project' , component : ProjectsComponent},
+  {path : '**' , pathMatch : 'full' , redirectTo : 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { UserEditComponent } from './components/user/user-edit/user-edit.component';
+import { ProjectComponent } from './components/projects/project/project.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    LoginComponent,
+    HomeComponent,
+    ProjectsComponent,
+    UserListComponent,
+    UserEditComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
