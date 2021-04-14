aeeda853545d2c414593c03c7114bd664687a213
Eric Janero <ejanerop97@gmail.com>
2021-04-14 19:56:15 -0400

    implemented project members delete

--- src/app/components/projects/project/project.component.ts
+++ src/app/components/projects/project/project.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GitUser } from 'src/app/models/git-user';
+import { ProjectService } from 'src/app/services/project.service';
+import { Dialog } from '../../dialog/dialog.component';
 
 @Component({
   selector: 'app-project',
@@ -7,15 +12,75 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
+  @Input() id : string = '';
   @Input() name : string = '';
   @Input() description : string = '';
   @Input() name_with_namespaces : string = '';
   @Input() avatar_url? : string = '';
   @Input() visible : boolean = true;
 
-  constructor() { }
+  members : GitUser[] = [];
+  initialized : boolean = false ;
+  displayedColumns: string[] = ['name', 'actions'];
+
+  constructor( private projectService : ProjectService, public dialog: MatDialog, private snack : MatSnackBar ) { }
 
   ngOnInit(): void {
+  }
+
+  initMembers() {
+    this.projectService.members( this.id ).subscribe(( resp : any ) => {
+      this.members = [];
+      console.log(resp);
+      this.initialized = true;
+      for (const item of resp.body) {
+        this.members.push(
+          new GitUser(
+            item.id,
+            item.name,
+            item.username,
+            item.state,
+            item.web_url,
+            item.avatar_url,
+            item.access_level
+          )
+        );
+      }
+    });
+  }
+
+
+  showMembers() {
+    if (!this.initialized) {
+      this.initMembers();
+    }
+  }
+
+  delete( user_id : string ) {
+
+    this.dialog.open(Dialog , {
+      data : {
+        title : 'Está seguro que quiere remover el usuario del proyecto?',
+        content : 'No tendrá acceso al proyecto.'
+      }
+    }).afterClosed().subscribe(confirmed => {
+      if(confirmed) {
+        this.projectService.deleteMember(this.id, user_id).subscribe(( resp : any ) =>{
+          console.log(resp);
+          this.initMembers();
+          this.openSnackBar('Usuario removido correctamente', 'Cerrar');
+        }, error => {
+          this.openSnackBar(error.error, 'Cerrar');
+        });
+      }
+    });
+
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snack.open(message, action, {
+      duration: 2000,
+    });
   }
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,7 +1,7 @@
+import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +17,19 @@ export class ProjectService {
     const url = `${this.url}/project`;
     const headers = this.auth.getAuthHeader();
     return this.http.get(url , { observe : 'response', headers : headers });
+  }
+
+  members( project : string )
+  {
+    const url = `${this.url}/project/${project}/members`;
+    const headers = this.auth.getAuthHeader();
+    return this.http.get(url , { observe : 'response', headers : headers });
+  }
+
+  deleteMember( project : string , user : string )
+  {
+    const url = `${this.url}/project/${project}/members/${user}`;
+    const headers = this.auth.getAuthHeader();
+    return this.http.delete(url , { observe : 'response', headers : headers });
   }
 }
