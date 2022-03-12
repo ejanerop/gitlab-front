e7a32085e722bd1d86d416e69dba26ca0ad031cf
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:53:23 -0500

    Format auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,14 +3,12 @@ import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor( private authService : AuthService, private router : Router ){}
-
-  canActivate(): boolean
-  {
+  canActivate(): boolean {
     if (!this.authService.isAuth()) {
       this.router.navigateByUrl('/login');
     }
