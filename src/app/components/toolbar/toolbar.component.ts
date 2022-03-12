ab588526f80b222bde2c616be6dbdeb20aabc355
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:52:48 -0500

    Format toolbar component

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -6,40 +6,37 @@ import { UtilService } from 'src/app/services/util.service';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.css'],
 })
 export class ToolbarComponent {
-
   constructor(
-    private authService : AuthService,
-    private util : UtilService,
-    private router : Router
-  ) { }
+    private authService: AuthService,
+    private util: UtilService,
+    private router: Router
+  ) {}
 
-  login()
-  {
+  login() {
     this.router.navigateByUrl('/login');
   }
 
-  logout()
-  {
-    this.authService.logout().subscribe( () => {
-      this.login();
-      this.util.openSnackBar('Cierre de sesión exitoso', 'Cerrar');
-    }, error => {
-      console.error(error);
-      if (error.status == 0) {
-        this.util.openSnackBar('Sin respuesta desde el servidor.', 'Cerrar');
-      }else {
-        this.util.openSnackBar(error.error, 'Cerrar');
+  logout() {
+    this.authService.logout().subscribe(
+      () => {
+        this.login();
+        this.util.openSnackBar('Cierre de sesión exitoso', 'Cerrar');
+      },
+      (error) => {
+        console.error(error);
+        if (error.status == 0) {
+          this.util.openSnackBar('Sin respuesta desde el servidor.', 'Cerrar');
+        } else {
+          this.util.openSnackBar(error.error, 'Cerrar');
+        }
       }
-    });
+    );
   }
 
-  isAuth()
-  {
+  isAuth() {
     return this.authService.isAuth();
   }
-
-
 }
