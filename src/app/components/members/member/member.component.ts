b44abfe0bd6b0b599cdde4b55ade82bd57f827bf
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:52:08 -0500

    Format member component

--- src/app/components/members/member/member.component.ts
+++ src/app/components/members/member/member.component.ts
@@ -7,29 +7,25 @@ import { Dialog } from '../../dialog/dialog.component';
 
 @Component({
   selector: 'app-member',
-  templateUrl: './member.component.html'
+  templateUrl: './member.component.html',
 })
 export class MemberComponent implements OnInit {
+  @Input() id: number = 0;
+  @Input() name: string = '';
+  @Input() username: string = '';
+  @Input() state: string = '';
+  @Input() avatar_url: string = '';
+  @Input() web_url: string = '';
+  @Input() visible: boolean = true;
+  @Input() access_level: number | undefined = 0;
 
-  @Input() id           : number               = 0;
-  @Input() name         : string               = '';
-  @Input() username     : string               = '';
-  @Input() state        : string               = '';
-  @Input() avatar_url   : string               = '';
-  @Input() web_url      : string               = '';
-  @Input() visible      : boolean              = true;
-  @Input() access_level : number | undefined   = 0;
+  @Output() remove: EventEmitter<string> = new EventEmitter();
 
-  @Output() remove      : EventEmitter<string> = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   removeMember() {
     this.remove.emit(this.id.toString());
   }
-
-
 }
