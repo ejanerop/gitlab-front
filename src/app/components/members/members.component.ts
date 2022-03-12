27117731f166dded6992197d409c3e8e057971fc
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:51:59 -0500

    Format members component

--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -8,100 +8,107 @@ import { MatDialog } from '@angular/material/dialog';
 import { UtilService } from 'src/app/services/util.service';
 @Component({
   selector: 'app-members',
-  templateUrl: './members.component.html'
+  templateUrl: './members.component.html',
 })
 export class MembersComponent implements OnInit {
-
-  form      : FormGroup = new FormGroup({});
-  groupName : string    = '';
-  loading   : boolean   = false;
-  users     : GitUser[] = [];
+  form: FormGroup = new FormGroup({});
+  groupName: string = '';
+  loading: boolean = false;
+  users: GitUser[] = [];
 
   constructor(
-    private groupService : GroupService,
-    private fb           : FormBuilder,
-    public dialog        : MatDialog,
-    private util         : UtilService
+    private groupService: GroupService,
+    private fb: FormBuilder,
+    public dialog: MatDialog,
+    private util: UtilService
   ) {
     this.form = this.fb.group({
-      'name' : ['']
+      name: [''],
     });
   }
 
-  get name()
-  {
+  get name() {
     return this.form.get('name')?.value;
   }
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.initGroup();
     this.refresh();
   }
 
-  delete( member : string )
-  {
-    this.dialog.open(Dialog, {
-      data : {
-        title : 'Está seguro que quiere remover el usuario del grupo?',
-        content : 'Se le removerá también el permiso a todos los proyectos del grupo.'
-      }
-    }).afterClosed().subscribe(confirmed => {
-      if(confirmed) {
-        this.loading = true;
-        this.groupService.deleteMember(environment.group_id, member ).subscribe( () => {
-          this.refresh();
-          this.util.openSnackBar('Usuario removido correctamente', 'Cerrar');
-        }, error => {
-          this.loading = false;
-          this.util.openSnackBar(error.error, 'Cerrar');
-        });
-      }
-    });
+  delete(member: string) {
+    this.dialog
+      .open(Dialog, {
+        data: {
+          title: 'Está seguro que quiere remover el usuario del grupo?',
+          content:
+            'Se le removerá también el permiso a todos los proyectos del grupo.',
+        },
+      })
+      .afterClosed()
+      .subscribe((confirmed) => {
+        if (confirmed) {
+          this.loading = true;
+          this.groupService
+            .deleteMember(environment.group_id, member)
+            .subscribe(
+              () => {
+                this.refresh();
+                this.util.openSnackBar(
+                  'Usuario removido correctamente',
+                  'Cerrar'
+                );
+              },
+              (error) => {
+                this.loading = false;
+                this.util.openSnackBar(error.error, 'Cerrar');
+              }
+            );
+        }
+      });
   }
 
-  refresh()
-  {
+  refresh() {
     this.loading = true;
-    this.groupService.members(environment.group_id).subscribe((resp : any) => {
-      this.users = [];
-      for (const item of resp.body) {
-        this.users.push(
-          new GitUser(
-            item.id,
-            item.name,
-            item.username,
-            item.state,
-            item.web_url,
-            item.avatar_url,
-            item.access_level ? item.access_level : 0
-          )
-        )
+    this.groupService.members(environment.group_id).subscribe(
+      (resp: any) => {
+        this.users = [];
+        for (const item of resp.body) {
+          this.users.push(
+            new GitUser(
+              item.id,
+              item.name,
+              item.username,
+              item.state,
+              item.web_url,
+              item.avatar_url,
+              item.access_level ? item.access_level : 0
+            )
+          );
+        }
+        this.loading = false;
+      },
+      (error) => {
+        console.error(error);
+        this.loading = false;
+        if (error.status == 0) {
+          this.util.openSnackBar('Sin respuesta desde el servidor.', 'Cerrar');
+        } else {
+          this.util.openSnackBar(error.error, 'Cerrar');
+        }
       }
-      this.loading = false;
-    }, error => {
-      console.error(error);
-      this.loading = false;
-      if (error.status == 0) {
-        this.util.openSnackBar('Sin respuesta desde el servidor.', 'Cerrar');
-      }else {
-        this.util.openSnackBar(error.error, 'Cerrar');
-      }
-    });
+    );
   }
 
-  initGroup()
-  {
-    this.groupService.group(environment.group_id).subscribe( ( resp : any ) => {
+  initGroup() {
+    this.groupService.group(environment.group_id).subscribe((resp: any) => {
       this.groupName = resp.body.name;
     });
   }
 
-  find()
-  {
-    this.users.forEach(user => {
+  find() {
+    this.users.forEach((user) => {
       user.checkName(this.name);
     });
   }
-
 }
