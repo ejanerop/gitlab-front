c0e7979962c3f3c24b3affa8a12205bb0296421d
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:34:34 -0500

    Format loading component

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -2,13 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
-  templateUrl: './loading.component.html'
+  templateUrl: './loading.component.html',
 })
 export class LoadingComponent {
+  @Input() loading: boolean = false;
+  @Input() height: number = 10;
 
-  @Input() loading : boolean = false;
-  @Input() height : number = 10;
-
-  constructor() { }
-
+  constructor() {}
 }
