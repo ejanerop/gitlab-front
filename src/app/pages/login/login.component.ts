d92410b3cc99a430f5bcff660353bfbd62645761
Eric Janero <ejanerop97@gmail.com>
2022-03-12 13:54:22 -0500

    Format login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,61 +7,60 @@ import { UtilService } from 'src/app/services/util.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
-  form    : FormGroup;
-  loading : boolean = false;
+  form: FormGroup;
+  loading: boolean = false;
 
   constructor(
-    private service : AuthService,
-    private fb : FormBuilder,
-    private util : UtilService,
-    private router : Router
+    private service: AuthService,
+    private fb: FormBuilder,
+    private util: UtilService,
+    private router: Router
   ) {
     this.form = new FormGroup({});
     this.createForm();
   }
 
-  createForm()
-  {
+  createForm() {
     this.form = this.fb.group({
-      name : ['', Validators.required],
-      password : ['', [Validators.required]],
+      name: ['', Validators.required],
+      password: ['', [Validators.required]],
     });
   }
 
-  openSnackBar( message: string, action: string )
-  {
+  openSnackBar(message: string, action: string) {
     this.util.openSnackBar(message, action);
   }
 
-  login()
-  {
-    if ( this.form.invalid ) {
-      Object.values(this.form.controls).forEach( ctrl => {
+  login() {
+    if (this.form.invalid) {
+      Object.values(this.form.controls).forEach((ctrl) => {
         ctrl.markAsTouched();
       });
       return;
     }
     this.loading = true;
-    this.service.login(this.form.value).subscribe( ( resp : any )=>{
-      this.loading = false;
-      this.router.navigateByUrl('/home');
-      this.service.saveInfo( resp.body.token, resp.body.user.name );
-      this.openSnackBar('Inicio de sesión exitoso', 'Cerrar');
-    },error => {
-      if (error.status == 422){
+    this.service.login(this.form.value).subscribe(
+      (resp: any) => {
         this.loading = false;
-        this.openSnackBar('Usuario y/o contraseña incorrectos', 'Cerrar');
-        Object.values(this.form.controls).forEach(ctrl => {
-          ctrl.setErrors({incorrect : true});
-        });
-      }else {
-        this.loading = false;
-        this.openSnackBar('Error en el servidor', 'Cerrar');
+        this.router.navigateByUrl('/home');
+        this.service.saveInfo(resp.body.token, resp.body.user.name);
+        this.openSnackBar('Inicio de sesión exitoso', 'Cerrar');
+      },
+      (error) => {
+        if (error.status == 422) {
+          this.loading = false;
+          this.openSnackBar('Usuario y/o contraseña incorrectos', 'Cerrar');
+          Object.values(this.form.controls).forEach((ctrl) => {
+            ctrl.setErrors({ incorrect: true });
+          });
+        } else {
+          this.loading = false;
+          this.openSnackBar('Error en el servidor', 'Cerrar');
+        }
       }
-    });
+    );
   }
 }
